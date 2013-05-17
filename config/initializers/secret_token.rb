c4f609a855cf4ac564657471bf81f7071b9b3d35
Prabhu Das <prabhu.das@clogeny.com>
2013-05-17 17:06:37 +0530

    Project files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseTestApp::Application.config.secret_token = '1b4416508a5290ad34d5e0212fff055ebba1acbace17960716ada0b8e88ce3376b059459bd7bcd53a30fa6d1ff49facb543c477705a04b48ff948b92786c95aa'
